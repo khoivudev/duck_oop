c453220f3c892f6e65fe5edda4b6d369a177dd74
khoivudev <vuhuykhoi1997@gmail.com>
2021-10-10 10:19:16 +0900

    [feat] Restructure project folder

--- src/FlyRocketPowered.java
+++ src/FlyRocketPowered.java
@@ -1,4 +1,4 @@
-public class FlyRocketPowered implements FlyBehavior{
+public class FlyRocketPowered implements FlyBehavior {
     public void fly() {
         System.out.println("I am flying with a rocket!");
     }
